183d7652a39aeb08aad538e2308c37483cc5cb43
Your Name <you@example.com>
2019-07-19 17:43:51 +0400

    Login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 // TODO: use --> import {Redirect} from 'react-router-dom';
-import {Container} from 'react-bootstrap';
+import {Container, Form, Button} from 'react-bootstrap';
 
 /**
  * Component for Login Page
  */
 export default class Login extends Component {
-  // constructor(props) {
-    // TODO: set state and form handlers
-  // }
+  constructor(props) {
+    //set state and form handlers
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+  handleSubmit(e){
+    e.preventDefault();
+  }
   render() {
     // TODO: use to redirect if user not logged in
     // if (this.props.user) {
@@ -22,7 +30,26 @@ export default class Login extends Component {
     // }
     return (
       <Container className="mt-3">
-        <div>TODO: create login form</div>
+        <Form onSubmit={this.handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text onChange={e => this.setState({email: e.target.value})} value={() => this.state.email} className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control onChange={e => this.setState({email: e.target.value})} value={() => this.state.password} type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicChecbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Log In
+        </Button>
+      </Form>
       </Container>
     )
   }
